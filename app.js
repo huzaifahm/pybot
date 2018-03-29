60e64b5734e17f5a9cb6f4f3855184bd2881c9e1
huzaifahm <34286369+huzaifahm@users.noreply.github.com>
2018-03-29 14:30:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Startup of bot
 //Load discord.js
 const Discord = require("discord.js");
 //New client
@@ -9,3 +10,12 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   client.user.setGame(`on ${client.guilds.size} servers`);
 });
+
+//Main functionality
+//Following command will now run on every message
+client.on("message", async message => {
+  if(message.author.bot) return;
+//To avoid a constant loop of bot return messages
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+//kick command
